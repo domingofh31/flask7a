2f367c54032403aa7b9d4aa9c8e722a68d7eebba
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2024-09-12 19:39:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    con.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    con.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
+    con.close()
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
 
 @app.route("/evento", methods=["GET"])
 def evento():
+    if not con.is_connected():
+        con.reconnect()
+
     pusher_client = pusher.Pusher(
         app_id="1714541",
         key="cda1cc599395d699a2af",
